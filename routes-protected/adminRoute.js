a5dc331e02d1fb0f68d5724e3674593a68b3cfbd
tahmid-404-20 <1905002@ugrad.cse.buet.ac.bd>
2024-03-03 02:38:47 +0600

    added admin product addition

--- routes-protected/adminRoute.js
+++ routes-protected/adminRoute.js
@@ -57,10 +57,61 @@ router.post("/support/update-ticket/make-read", (req, res) => {
 
 // send {ticketId: 1234, status: "closed", comment: "lorem ipsum" }
 router.post("/support/update-ticket/close-ticket", (req, res) => {
-  const updateStatusUrl = process.env.adminUrl + "/support/update-ticket/update-status";
+  const updateStatusUrl =
+    process.env.adminUrl + "/support/update-ticket/update-status";
 
   axios
-    .post(updateStatusUrl, { id: req.user.id, ticketId: req.body.ticketId, status: req.body.status, comment: req.body.comment })
+    .post(updateStatusUrl, {
+      id: req.user.id,
+      ticketId: req.body.ticketId,
+      status: req.body.status,
+      comment: req.body.comment,
+    })
+    .then((response) => {
+      res.status(200).send(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(404).send({ message: "Not found" });
+    });
+});
+
+// admin product calls
+router.get("/products", (req, res) => {
+  const productsUrl = process.env.adminUrl + "/product/get-products";
+
+  axios
+    .post(productsUrl, { id: req.user.id })
+    .then((response) => {
+      res.status(200).send(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(404).send({ message: "Not found" });
+    });
+});
+
+// req.body = {name, unit, unit_price, tax_amount, image_link}
+router.post("/product/add-product", (req, res) => {
+  const addProductUrl = process.env.adminUrl + "/product/add-product";
+
+  axios
+    .post(addProductUrl, req.body)
+    .then((response) => {
+      res.status(200).send(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(404).send({ message: "Not found" });
+    });
+});
+
+// {id, unit_price, tax_amount}
+router.post("/product/update-product", (req, res) => {
+  const updateProductUrl = process.env.adminUrl + "/product/update-product";
+
+  axios
+    .post(updateProductUrl, req.body)
     .then((response) => {
       res.status(200).send(response.data);
     })
@@ -71,6 +122,5 @@ router.post("/support/update-ticket/close-ticket", (req, res) => {
 });
 
 // checking the loan history of a user
-
 
 module.exports = router;
